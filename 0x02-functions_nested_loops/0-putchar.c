37523dc281febac89fcd5c0ac8bad4ca0355d3dd
gladdayijokgwung <glanzyemmy@gmail.com>
2022-09-13 17:34:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar(รร'u');
+	_putchar('t');
+	_putchar(ร'c');
+	_putchar('h');
+	_putchar(ร'a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my function, nested loops readme
