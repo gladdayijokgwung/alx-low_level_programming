fb316cc3124cd2981ba442f961272355c6b8d121
gladdayijokgwung <glanzyemmy@gmail.com>
2022-09-15 17:42:44 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,20 +14,19 @@ int main(void)
 
 	while (num++ < 100)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		
+		if ((num % 3 == 0) && (num % 5 == 0))	
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if ((num % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
 			{
-				printf("Buzz ");
+				printf("Buzz");
 			}
 			else
 			{
@@ -36,7 +35,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%d", num);
 		}
 	}
 	printf("\n");
